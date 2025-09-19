25f59b53f58bb1099b9dec4c42776d05cb6457e6
Will Medrano <will@wmedrano.dev>
2025-09-19 14:22:37 -0700

    Add build rule for generating docs

--- build.zig
+++ build.zig
@@ -19,4 +19,13 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    const lib_docs = b.addInstallDirectory(.{
+        .source_dir = lib_unit_tests.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&lib_docs.step);
 }
